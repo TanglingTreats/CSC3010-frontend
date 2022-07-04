6bd72eafbc69f78e2a4f5ea25eac6b453e729443
Edwin Tang <edwintang10@gmail.com>
2022-07-04 16:08:03 +0800

    Add routing in App. Add search bar component

---
+++ src/SearchBar/searchBar.tsx
@@ -0,0 +1,21 @@
+import React, {useState} from "react";
+
+import {ReactComponent as MagnifyingGlass }from '../assets/magnifying-glass-search-svgrepo-com.svg';
+
+interface SearchBarIntf {
+  sendQuery: any;
+}
+
+export function SearchBar(props: SearchBarIntf) {
+  const [searchTerm, setSearchTerm] = useState<string>();
+
+  function sendQuery() {
+    props.sendQuery(searchTerm)
+  }
+
+  return (<div className="search-bar">
+    <input id="" size={30} type="search" onChange={(e)=>{setSearchTerm(e.target.value);}} onKeyUp={(e) => {if(e.key === "Enter") sendQuery();}} tabIndex={0} autoFocus></input>
+    <button className="search-button" onClick={() => sendQuery()}><MagnifyingGlass/></button>
+  </div>);
+
+}
