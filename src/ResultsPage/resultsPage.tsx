2e068e259bdbcedc7b603c3776022ca80d37072a
Edwin Tang <edwintang10@gmail.com>
2022-07-04 18:11:09 +0800

    Add results page header. Add default query for search bar.

--- src/ResultsPage/resultsPage.tsx
+++ src/ResultsPage/resultsPage.tsx
@@ -1,14 +1,28 @@
 import React from "react";
+import { useNavigate, useSearchParams } from "react-router-dom";
+import { SearchBar } from "../SearchBar/searchBar"
+
+import "./resultsPage.css";
 
 interface ResultsPageProps {
   appName: string
 }
 
 export function ResultsPage(props: ResultsPageProps) {
+  const navigate = useNavigate();
+  let [searchParams, setSearchParams] = useSearchParams();
+
+  const query = searchParams.get("query") || "";
+
+  function sendQuery(query: string) {
+    navigate(`/search?query=${query}`)
+  }
+
   return (
     <div className="page">
-      <header>
-        <h2>props.appName</h2>
+      <header className="header">
+        <h2>{props.appName}</h2>
+        <SearchBar query={query} sendQuery={sendQuery} />
       </header>
       <div className="content">
       </div>
