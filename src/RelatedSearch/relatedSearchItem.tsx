66234e468b0687594ec3dde504de9c43ca7cb1b0
Edwin Tang <edwintang10@gmail.com>
2022-07-22 17:35:44 +0800

    Add disable clicking selected facet.

--- src/RelatedSearch/relatedSearchItem.tsx
+++ src/RelatedSearch/relatedSearchItem.tsx
@@ -5,14 +5,18 @@ interface RelatedSearchItemIntf {
   title: string;
   category: string;
   sendQuery: any;
+  selectedFacet: string;
 }
 
 export function RelatedSearchItem(props: RelatedSearchItemIntf) {
 
   const indexToSlice = props.title.lastIndexOf(" ");
   const facetValue = props.title.slice(0, indexToSlice);
+
+  const isSelected = facetValue === props.selectedFacet;
+
   return (
-    <div className="related-search" style={{cursor: "pointer"}} onClick={() => props.sendQuery(props.category, facetValue)}>
+    <div className={`related-search ${isSelected ? "selected" : "select"}`} onClick={() => {if (!isSelected) props.sendQuery(props.category, facetValue)}}>
       <MagnifyingGlass width="20px"/>
       <p>{props.title}</p>
     </div>
