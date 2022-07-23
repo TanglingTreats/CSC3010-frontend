1dcb7362b35001ad44f1b5614a3f331c10fdecd0
Edwin Tang <edwintang10@gmail.com>
2022-07-23 15:07:48 +0800

    Add removal of facets list after clicking on one of the facets.

--- src/common/makeAPICall.ts
+++ src/common/makeAPICall.ts
@@ -3,18 +3,10 @@ import axios from "axios";
 //const https = require("https");
 //const http = require("http");
 
-const useBase = process.env.USE_BASE === "true";
 
 // Choose to use local or server url
 const api_base_url = process.env.REACT_APP_ENDPOINT || "";
-  //process.env.USE_CUSTOM === "true"
-    //? process.env.LOCAL_DEV_API
-    //: process.env.DEV_API_DOMAIN || "base_url";
 
-const api_proxy_url = process.env.DEV_API_PROXY || "proxy";
-// const api_prod_url = process.env.REACT_APP_PROD_API_DOMAIN;
-//
-// Choose to use base url or proxy url
 const api_url = api_base_url;
 
 const axiosInstance = axios.create({
@@ -55,9 +47,3 @@ export default async function makeAPICall(options: any) {
 
   return response;
 }
-
-const graph_base_api = process.env.REACT_APP_GRAPH_ENDPOINT;
-
-const axiosInstanceTeams = axios.create({
-  baseURL: graph_base_api,
-});
